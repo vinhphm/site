12f24e5276919489c6b3bdc2b2f70c9c5a01bea6
Vinh Pham <v@vinh.dev>
2026-03-11 02:15:50 +0000

    Upgrade to Astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -73,6 +73,10 @@ export default defineConfig({
   experimental: {
     contentIntellisense: true,
     svgo: true,
+    queuedRendering: {
+      enabled: true,
+    },
+    rustCompiler: true,
   },
 
   integrations: [
